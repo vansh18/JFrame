50c558a5e0cbc51047394ac22cd21aa0f6611867
Vansh <vansh.gupta850@gmail.com>
2022-12-18 14:55:56 +0400

    added label

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 import javax.swing.*;
-import java.awt.*;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        JLabel label = new JLabel();
+        label.setText("this is my label");
+
         JFrame frame = new JFrame();
         frame.setTitle("Flight Finder");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Set the default function on close
-        frame.setResizable(false); // Can't resize
+//        frame.setResizable(false); // Can't resize
         frame.setSize(420,420);
         frame.setVisible(true);
         ImageIcon image = new ImageIcon("LOGO.png");
         frame.setIconImage(image.getImage()); // Change icon of frame
-        frame.getContentPane().setBackground(new Color(5,10,25)); // Changes the color
+//        frame.getContentPane().setBackground(new Color(5,10,25)); // Changes the color
+        frame.add(label);
+        ImageIcon img = new ImageIcon("LOGO.png");
+        label.setIcon(img);
+
     }
 }
