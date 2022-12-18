3aa528be3976db0df822dedccde55993697613be
Vansh <vansh.gupta850@gmail.com>
2022-12-18 14:35:54 +0400

    added frame

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main
 {
     public static void main(String[] args)
     {
         JFrame frame = new JFrame();
+        frame.setTitle("Flight Finder");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Set the default function on close
+        frame.setResizable(false); // Can't resize
         frame.setSize(420,420);
         frame.setVisible(true);
+        ImageIcon image = new ImageIcon("LOGO.png");
+        frame.setIconImage(image.getImage()); // Change icon of frame
+        frame.getContentPane().setBackground(new Color(5,10,25)); // Changes the color
     }
 }
