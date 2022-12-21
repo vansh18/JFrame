1d53a6b37f1748d68da7d9ffc73c17b4ce650829
Vansh <vansh.gupta850@gmail.com>
2022-12-21 12:29:18 +0400

    Prime number checker

---
+++ src/PrimeNo_GUI.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class PrimeNo_GUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Prime_GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final int FIELD_WIDTH = 20;
+        frame.setSize(420,420);
+        final JTextField numField = new JTextField(FIELD_WIDTH);
+        final JTextField resField = new JTextField(FIELD_WIDTH);
+        JButton checkPrimeButton = new JButton("NICE");
+        checkPrimeButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event)
+            {
+                int num = Integer.parseInt(numField.getText());
+                boolean flag = true;
+                int endNo = num - 1;
+                for (int i=2; i <= endNo; i++ )
+                {
+                    if ( num % i == 0)
+                    {
+                        flag = false; break;
+                    }
+                }
+                resField.setText("Prime = " + flag);
+
+            }
+        });
+        frame.setLayout(new FlowLayout());
+        frame.add(numField);
+        frame.add(checkPrimeButton);
+        frame.add(resField);
+        frame.setVisible(true);
+    }
+}
