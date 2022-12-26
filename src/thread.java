fe56b0481663aa21435ce7fc425250b3fc804bb7
Vansh <vansh.gupta850@gmail.com>
2022-12-26 22:47:32 +0400

    threading

---
+++ src/thread.java
@@ -0,0 +1,68 @@
+class multithreadimplements implements Runnable
+{
+    private int threadNum;
+    public int sum_num=0;
+    private int start;
+    private int end;
+
+    public multithreadimplements(int n,int start,int end)
+    {
+        this.threadNum = n;
+        this.start = start;
+        this.end = end;
+    }
+
+    public void run()
+    {
+        for(int i = start; i<=end; i++)
+        {
+            sum_num += i;
+        }
+    }
+}
+
+public class thread
+{
+    public static void main(String args[])
+    {
+        int num=10;
+        int n=5;
+
+        int d = num / n;
+
+        int start = 1;
+        int end = 0;
+
+        multithreadimplements [] arr = new multithreadimplements[n];
+
+        int ans=0;
+
+
+        for(int i = 0;i<n;i++)
+        {
+            end = end+d;
+
+            arr[i] = new multithreadimplements(i,start,end);
+            Thread myThread = new Thread(arr[i]);
+
+            myThread.start();
+            start = start+d;
+
+            if(i==n-1)
+            {
+                try
+                {
+                    myThread.join();
+                }
+                catch(InterruptedException e){}
+            }
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            ans += arr[i].sum_num;
+        }
+
+        System.out.println(ans);
+    }
+}
